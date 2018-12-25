053a2692a3cca062d20a698499a98bd951889db1
Joe Mumm <jmummdbd@att.net>
2018-12-24 22:34:11 -0800

    HTML, CSS, JavaScript, NPM & jQuery

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+  
+  console.log("Hello Pluralsight");
+
+  var theForm = $("#theForm");
+    //document.getElementById("theForm");
+  //theForm.hidden = true;
+  theForm.hide();
+
+  var button = $("#buyButton");//document.getElementById("buyButton");
+  button.on("click", function () { //addEventListener
+    console.log("Buying Item");
+  });
+
+  var productProps = $(".product-props li");// document.getElementsByClassName("product-props");
+  //var listItems = productProps.item[0].children;
+    productProps.on("click", function () {
+      console.log("You clicked on " + $(this).text());
+  });
+
+  var $loginToggle = $("#loginToggle");
+  var $popupForm = $(".popup-form");
+
+  $loginToggle.on("click", function () {
+    $popupForm.fadeToggle(500);
+  });
+});
