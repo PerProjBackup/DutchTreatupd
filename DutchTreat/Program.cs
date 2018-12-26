afd37f568796cce91e972e035ec0f2cfcbcf1949
Joe Mumm <jmummdbd@att.net>
2018-12-26 12:06:44 -0800

    Entity Framework Core, Seeding, Repository and Logging

--- DutchTreat/Program.cs
+++ DutchTreat/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace DutchTreat
@@ -14,11 +16,36 @@ namespace DutchTreat
   {
     public static void Main(string[] args)
     {
-      CreateWebHostBuilder(args).Build().Run();
+      var host = CreateWebHostBuilder(args).Build();
+      RunSeeding(host);
+        
+        host.Run();
+    }
+
+    private static void RunSeeding(IWebHost host)
+    {
+      var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+
+      using (var scope = scopeFactory.CreateScope())
+      {
+        var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
+        seeder.Seed();
+      }
     }
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
+      WebHost.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration(SetupConfiguration)
             .UseStartup<Startup>();
+
+    private static void SetupConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder builder)
+    {
+      // Removing the default configuration options
+      builder.Sources.Clear();
+      builder.AddJsonFile("config.json", false, true)
+        //.AddXmlFile("config.xml", true)
+        .AddEnvironmentVariables();
+
+    }
   }
 }
