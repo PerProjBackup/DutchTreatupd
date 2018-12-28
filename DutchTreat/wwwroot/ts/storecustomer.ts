57f5a8f464c5262f6a5236914fd2b7abc443c0fa
Joe Mumm <jmummdbd@att.net>
2018-12-28 12:02:23 -0800

    TypeScript Intro

---
+++ DutchTreat/wwwroot/ts/storecustomer.ts
@@ -0,0 +1,37 @@
+﻿// export
+class StoreCustomer {
+
+  //constructor(theName: string) {
+  //  this.name = theName;
+  //}
+
+  constructor(private firstName:string, private lastName:string) {
+
+  }
+
+  public visits: number = 0;
+  private ourName: string;
+
+  //public showName(name: string): boolean {
+  //  alert(name);
+  //  return true;
+  //}
+  public showName() {
+   // alert(this.name);
+    alert(this.firstName + " " + this.lastName);
+  }
+
+  set name(val) {
+    this.ourName = val;
+  }
+
+  get name() {
+    return this.ourName;
+  }
+}
+
+
+
+//let cust = new StoreCustomer();
+////cust.visits = "dsdfdsaff";
+//cust.visits = 10;
